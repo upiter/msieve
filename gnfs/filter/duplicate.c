5cd9d712c32742e035d5f2702dcf6c0a6cfd589e
jasonp_sf <jasonp_sf@8c02911e-0617-4013-80d0-c3bcbcae04d5>
2010-07-19 01:25:48 +0000

    don't correct for a compressed savefile in windows
    git-svn-id: https://svn.code.sf.net/p/msieve/code/trunk@337 8c02911e-0617-4013-80d0-c3bcbcae04d5

--- gnfs/filter/duplicate.c
+++ gnfs/filter/duplicate.c
@@ -300,11 +300,13 @@ uint32 nfs_purge_duplicates(msieve_obj *obj, factor_base_t *fb,
 	log2_hashtable1_size = 28;
 	if (rel_size > 0.0) {
 		double num_rels; /* estimated */
-		if(savefile->isCompressed) {
+#if !defined(WIN32) && !defined(_WIN64)
+		if (savefile->isCompressed) {
 			char name_gz[256];
 			sprintf(name_gz, "%.gz", savefile->name);
 			num_rels = get_file_size(name_gz) / 0.55 / rel_size;
 		} else 
+#endif
 			num_rels = get_file_size(savefile->name) / rel_size;
 		log2_hashtable1_size = log(num_rels * 10.0) / M_LN2 + 0.5;
 	}
