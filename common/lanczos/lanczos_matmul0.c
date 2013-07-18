199d2b15f237cf43137761762347f3d4fe268ee7
jasonp_sf <jasonp_sf@8c02911e-0617-4013-80d0-c3bcbcae04d5>
2013-07-18 00:25:41 +0000

    remove fixed vsize; there are too many places where it would not apply
    git-svn-id: https://svn.code.sf.net/p/msieve/code/trunk@920 8c02911e-0617-4013-80d0-c3bcbcae04d5

--- common/lanczos/lanczos_matmul0.c
+++ common/lanczos/lanczos_matmul0.c
@@ -428,7 +428,6 @@ void packed_matrix_init(msieve_obj *obj,
 	p->start_col = start_col;
 	p->num_dense_rows = num_dense_rows;
 	p->num_threads = 1;
-	p->vsize = ncols;
 #ifdef HAVE_MPI
 	p->mpi_size = obj->mpi_size;
 	p->mpi_nrows = obj->mpi_nrows;
@@ -486,7 +485,6 @@ void packed_matrix_init(msieve_obj *obj,
 				obj->cache_size2 / 1024);
 
 	p->num_threads = num_threads = MIN(num_threads, MAX_THREADS);
-	p->vsize = ncols / num_threads;
 	p->unpacked_cols = NULL;
 	p->first_block_size = first_block_size;
 
